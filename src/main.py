a10af16d5a8265e6742e2e324a39d955d370549a
Chris Legg <chris@chrislegg.net>
2024-10-02 17:59:23 +0100

    Add initial object to hold textnode data and test in main

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode
+def main():
+    text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(text_node.__repr__())
+
+if __name__=="__main__":
+    main()
